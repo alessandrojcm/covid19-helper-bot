c73a35a1b930c905622c0cc727bb0618df52bce8
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-08 21:23:13 -0400

    build: tweaked travis yml

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python main.py prepare-schema
+pip install poetry
+poetry export -f requirements.txt > requirements.txt
+pip install -r requirements.txt
 npm i -g twilio-cli
 twilio plugins:install @dabblelab/plugin-autopilot
 twilio autopilot:update -s=./assistant/schema_ready.json --unique-name=TwilioHackathon
--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip install poetry
-poetry export -f requirements.txt > requirements.txt
-pip install -r requirements.txt
