0ddeed5cce055d9f77947691c93648c7b7da8d51
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-08 23:36:39 -0400

    fix: slash missing for prefixes

--- app/models/config.py
+++ app/models/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseSettings, AnyHttpUrl, validator
 from pydantic.dataclasses import dataclass
 from pydantic.types import SecretStr
@@ -8,8 +10,8 @@ from app.models.logging_levels import LoggingLevels
 
 @dataclass()
 class Config(BaseSettings):
-    API_PREFIX: str = "api"
-    AUTOPILOT_ENDPOINT_PREFIX: str = "autopilot"
+    API_PREFIX: str = "/api"
+    AUTOPILOT_ENDPOINT_PREFIX: str = "/autopilot"
     VERSION: str = "0.1.0"
     DEBUG: bool = False
     TESTING: bool = False
@@ -19,6 +21,7 @@ class Config(BaseSettings):
     FAUNA_DB_URL: AnyHttpUrl = "http://localhost:8443"
     FAUNA_SERVER_KEY: SecretStr = "your_server_key_here"
     TWILIO_ENDPOINT: AnyHttpUrl = "http://localhost:5000"
+    SENTRY_DSN: Optional[AnyHttpUrl]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
