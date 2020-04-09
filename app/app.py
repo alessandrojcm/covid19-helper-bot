06b6e8d8f7ba36059a22cf7f1530ede197dbd650
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-08 23:36:28 -0400

    feat: added sentry

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api import router
+from app.core.environments import Environments
 
 
 def get_application(config) -> FastAPI:
@@ -10,4 +11,14 @@ def get_application(config) -> FastAPI:
     application.get("")
     application.include_router(router, prefix=config.API_PREFIX)
 
-    return application
+    if config.ENVIRONMENT == Environments.DEV:
+        return application
+    return sentry_handler(config)(application)
+
+
+def sentry_handler(config):
+    import sentry_sdk
+    from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
+    sentry_sdk.init(dsn=config.SENTRY_DSN)
+    return SentryAsgiMiddleware
