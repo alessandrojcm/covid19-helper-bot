d20aeba82792c7b45d84f62494ec096694f1464d
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-08 23:50:20 -0400

    build: fixed autopilot bot name

--- after_deploy.sh
+++ after_deploy.sh
@@ -5,4 +5,4 @@ poetry export -f requirements.txt > requirements.txt
 pip install -r requirements.txt
 npm i -g twilio-cli
 twilio plugins:install @dabblelab/plugin-autopilot
-twilio autopilot:update -s=./assistant/schema_ready.json --unique-name=TwilioHackathon
+twilio autopilot:update -s=./assistant/schema_ready.json --unique-name=COVID19-Bot
