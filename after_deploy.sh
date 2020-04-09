341077f973e3582424a823c2cc01f3a0e0819309
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-09 01:10:06 -0400

    build: removed travis in favor of buddy.works

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Installing requirements for cli
-pynv install -v 3.8.2
 pip install poetry
 python -m poetry export -f requirements.txt > requirements.txt
 pip install --user -r requirements.txt
 
 # Installing twilio cli
-nvm install 13.12.0
-nvm use 13.12.0
 npm i -g twilio-cli
 
 # Generating schema
