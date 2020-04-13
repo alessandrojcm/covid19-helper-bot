5278254166a891a85a96f9f9f67c2e276447df29
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-12 20:24:31 -0400

    fix: twilio validator bypass on dev

--- app/dependencies/twilio_request_validator.py
+++ app/dependencies/twilio_request_validator.py
@@ -2,11 +2,14 @@ from fastapi import Header, Request, HTTPException
 from twilio.request_validator import RequestValidator
 
 from app.core import config
+from app.models.environments import Environments
 
 
 async def twilio_request_validator(
     request: Request, x_twilio_signature: str = Header(...)
 ):
+    if config.ENVIRONMENT == Environments.DEV:
+        return request
     validator = RequestValidator(config.TWILIO_AUTH_TOKEN)
 
     is_valid = validator.validate(
