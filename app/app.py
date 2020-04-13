e431e2893399d1e3b366f5d3883e5ded361a4b39
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-12 20:29:06 -0400

    fix: uncommented middlewares

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from app.api import router
-from app.models.environments import Environments
 from app.middlewares import MIDDLEWARES
+from app.models.environments import Environments
 from app.services import init_sentry
 
 
@@ -13,7 +13,6 @@ def get_application(config) -> Union[FastAPI, SentryAsgiMiddleware]:
     application = FastAPI(
         title=config.PROJECT_NAME, debug=config.DEBUG, version=config.VERSION
     )
-    application.get("")
     application.include_router(router, prefix=config.API_PREFIX)
 
     if config.ENVIRONMENT == Environments.DEV:
@@ -23,5 +22,5 @@ def get_application(config) -> Union[FastAPI, SentryAsgiMiddleware]:
 
 
 def init_middlewares(application):
-    # [application.add_middleware(middleware) for middleware in MIDDLEWARES]
+    [application.add_middleware(middleware) for middleware in MIDDLEWARES]
     init_sentry()
