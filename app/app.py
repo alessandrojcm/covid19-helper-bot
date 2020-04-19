9aa6c2fce707f9b01dbcb8b02efcaedb3fb1d433
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-04-19 00:51:52 -0400

    refactor: changed error handler decorator for function as per fastapi docs

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from app.api import router
 from app.middlewares import MIDDLEWARES
 from app.models.environments import Environments
 from app.services import init_sentry
+from app.error_handlers import error_fallback_action
 
 
 def get_application(config) -> Union[FastAPI, SentryAsgiMiddleware]:
@@ -14,6 +16,7 @@ def get_application(config) -> Union[FastAPI, SentryAsgiMiddleware]:
         title=config.PROJECT_NAME, debug=config.DEBUG, version=config.VERSION
     )
     application.include_router(router, prefix=config.API_PREFIX)
+    application.exception_handler(HTTPException)(error_fallback_action)
 
     if config.ENVIRONMENT == Environments.DEV:
         return application
